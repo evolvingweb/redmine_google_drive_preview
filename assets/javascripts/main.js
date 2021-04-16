f01bc3cce89ac4790e6ed90fd0c5a1076f7f6a0d
Kevin Porras <kporras07@gmail.com>
2021-04-16 11:36:01 -0600

    Insert macro with editor button.

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -13,11 +13,14 @@
             var gDriveEmbedDialog = '<div id="gDriveEmbedDialog" title="Insert Google Drive document URL"></div>';
             $("#main").append(gDriveEmbedDialog);
             $('#gDriveEmbedDialog').append('<input type="text" class="gDriveEmbed-text"/>');
+            $('#gDriveEmbedDialog').append('<span class="gDriveEmbed-help">Remember to set the right access permissions to the document that you are sharing.</span>');
+
             $('#gDriveEmbedDialog').append('<button class="gDriveEmbed-button">Embed</button>');
             $('.gDriveEmbed-button').click(function() {
               var url = $('.gDriveEmbed-text').val();
-              textField.encloseLineSelection(url, '');
-              $('.gDriveEmbed-textarea').val('');
+              var macro = "{{google_drive_embed(" + url + ")}}"
+              textField.encloseLineSelection(macro, '');
+              $('.gDriveEmbed-text').val('');
               $('#gDriveEmbedDialog').dialog('close');
             });
             $("#gDriveEmbedDialog").dialog({modal:true, width:'600px'});
